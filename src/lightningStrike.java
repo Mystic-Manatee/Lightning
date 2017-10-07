1a9cdf8f06d25136a560a827815fbdc9458ca33b
Joshua <30722988+Mystic-Manatee@users.noreply.github.com>
2017-10-07 11:46:05 +1000

    changed the generation from a line to a ramdonmised pattern

--- src/lightningStrike.java
+++ src/lightningStrike.java
@@ -6,6 +6,9 @@ public class lightningStrike extends JPanel{
 		super.paintComponent(g);
 		this.setBackground(Color.BLACK);
 		
+		int m;
+		int startLocation = 180;
+		
 		g.setColor(Color.WHITE);
 		
 		//iterate through each row and col in the panel
@@ -14,11 +17,18 @@ public class lightningStrike extends JPanel{
 		for (int i = 0; i < 480; i++) {
 			//iterate through the x axis
 			for (int j = 0; j < 640; j++) {
-				if(j == 180) {
-					g.drawLine(j, i, j, i);
+				//sets a random int 0 or 1 and based on that either stays in the same course of offsets by 1
+				m = ((int)(Math.random()*2));
+				if(j == startLocation) {
+					if(m == 1) {
+						startLocation += 1;
+					}
+					else {
+						startLocation -= 1;
+					}
+					g.drawLine(startLocation, i, startLocation, i);
 				}
 			}
 		}
 	}
-	
 }
